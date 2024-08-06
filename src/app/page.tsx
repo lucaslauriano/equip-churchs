a37087a4e4ae7833b757a9070215fa73ef467e81
Lucas Lauriano de Souza <lucaslauriano.souza@gmail.com>
2024-08-06 10:15:08 -0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
-// components
-import { Navbar, Footer } from "@/components";
+import { Navbar, Footer } from '@/components';
 
-// sections
-import Hero from "./hero";
-import OnlineCourse from "./online-course";
-import WhyChooseUs from "./why-choose-us";
-import CarouselFeatures from "./carousel-features";
-import Pricing from "./pricing";
-import OtherCourses from "./other-courses";
+import Hero from './hero';
+import OnlineCourse from './online-course';
+import WhyChooseUs from './why-choose-us';
+import CarouselFeatures from './carousel-features';
+import Pricing from './pricing';
+import OtherCourses from './other-courses';
 
 export default function Campaign() {
   return (
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,5 @@
-"use client";
-
-import React from "react";
-import { ThemeProvider } from "@material-tailwind/react";
+import React from 'react';
+import { ThemeProvider } from '@material-tailwind/react';
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return <ThemeProvider>{children}</ThemeProvider>;
